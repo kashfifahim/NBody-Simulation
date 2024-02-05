3bb54beafe1147bdcb4f774a8259a03a6b8954ee
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2024-02-05 11:26:24 -0500

    implemented main function, read and parse args

--- NBody.java
+++ NBody.java
@@ -24,4 +24,13 @@ public class NBody {
         }
         return bodies;
     }
+
+    public static void main(String[] args) {
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        double radius = readRadius(filename);
+        Body[] bodies = readBodies(filename);
+    
+    }
 }

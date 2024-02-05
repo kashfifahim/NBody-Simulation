cbffddf6f2c06540cd8b815bf0d5997433272e12
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2024-02-05 11:02:33 -0500

    implemented readBodies() method in NBody class, passed unitTest

--- NBody.java
+++ NBody.java
@@ -5,4 +5,23 @@ public class NBody {
         double radius = in.readDouble();
         return radius;
     }
+
+    public static Body[] readBodies(String filename) {
+        In in = new In(filename);
+        int numberOfPlanets = in.readInt();
+        Body[] bodies = new Body[numberOfPlanets];
+        System.out.print(bodies);
+        double radius = in.readDouble();
+        for (int i = 0; i < numberOfPlanets; i++) {
+            double xxPos = in.readDouble();
+            double yyPos = in.readDouble();
+            double xxVel = in.readDouble();
+            double yyVel = in.readDouble();
+            double mass = in.readDouble();
+            String imgFileName = in.readString();
+            Body b = new Body(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
+            bodies[i] = b;
+        }
+        return bodies;
+    }
 }
